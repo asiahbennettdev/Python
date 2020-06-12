8931d44a9f2e6f859841b8647e369d306163ae64
Asiah Bennett Dev <asiahbennettdev@gmail.com>
2020-06-11 22:49:36 -0400

    string methods

--- string methods/stringmethods.py
+++ string methods/stringmethods.py
@@ -24,4 +24,4 @@ print(hook.rfind('Wish'))
 
 
 
-                                                                                                                                                                                                                                                                                                                            
+                                                                                                                                                                                                                                                                                                                        
